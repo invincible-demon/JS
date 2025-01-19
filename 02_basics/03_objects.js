5b55bf669cfcf1bd71fbbaa9e3ab7934394f9b01
invincible-demon <123808875+invincible-demon@users.noreply.github.com>
2025-01-19 14:04:34 +0000

    objects config done

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -7,12 +7,12 @@ const mySym = Symbol("key1")
 
 
 const JsUser = {
-    name: "Hitesh",
-    "full name": "Hitesh Choudhary",
+    name: "Ashish",
+    "full name": "Ashish Gupta",
     [mySym]: "mykey1",
     age: 18,
     location: "Jaipur",
-    email: "hitesh@google.com",
+    email: "ashish@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
@@ -22,9 +22,9 @@ const JsUser = {
 // console.log(JsUser["full name"])
 //console.log(JsUser[mySym])
 
-JsUser.email = "hitesh@chatgpt.com"
+JsUser.email = "ashish@chatgpt.com"
 // Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
+JsUser.email = "ashish@microsoft.com"
 // console.log(JsUser);
 
 JsUser.greeting = function(){
